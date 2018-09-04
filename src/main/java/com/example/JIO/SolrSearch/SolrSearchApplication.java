5dc95ff2dc69a3223ccd34476e29ee36687e515e
keshavjoshi142 <keshavjoshi142@gmail.com>
2018-09-04 12:40:30 +0530

    Started

---
+++ src/main/java/com/example/JIO/SolrSearch/SolrSearchApplication.java
@@ -0,0 +1,12 @@
+package com.example.JIO.SolrSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrSearchApplication.class, args);
+	}
+}
