cf20feadf8b7c4aa2274b5207430c82bd4db8538
keshavjoshi142 <keshavjoshi142@gmail.com>
2018-09-07 18:03:03 +0530

    partial Autocomplete done...

--- src/main/java/com/example/JIO/SolrSearch/Config/SolrConfig.java
+++ src/main/java/com/example/JIO/SolrSearch/Config/SolrConfig.java
@@ -4,28 +4,25 @@ package com.example.JIO.SolrSearch.Config;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.repository.config.EnableSolrRepositories;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @Configuration
-@EnableSolrRepositories(basePackages = "com.example.JIO.SolrSearch.Repository" ,
-                namedQueriesLocation = "classpath:application.properties")
+@EnableSolrRepositories(basePackages = "com.example.JIO.SolrSearch.Repository",
+        namedQueriesLocation = "classpath:application.properties")
 @ComponentScan
 public class SolrConfig {
 
     @Bean
-    public SolrClient solrClient()
-    {
+    public SolrClient solrClient() {
         return new HttpSolrClient("http://localhost:8985/solr");
     }
 
     @Bean
-    public SolrTemplate solrTemplate(SolrClient client) throws Exception
-    {
+    public SolrTemplate solrTemplate(SolrClient client) throws Exception {
         return new SolrTemplate(client);
     }
 }
